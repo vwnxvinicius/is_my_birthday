2bf05afd101211efca4a36e3a7394782681dba24
vwnxvinicius <vwnx123@gmail.com>
2021-08-10 07:59:38 -0300

    Começando a adicionar o front-end

--- birth_app/main/views.py
+++ birth_app/main/views.py
@@ -1,6 +1,22 @@
+from django.forms.forms import Form
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+
+class BirthDate(forms.Form):
+    day = forms.IntegerField(label="Your birth day", min_value=1, max_value=31)
+
+def get_day(request):
+    if request.method == 'POST':
+        form = BirthDate(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('')
+    else:
+        form = BirthDate()
+    return render(request, "birth/index.html", {"form": form})
+
 def home(request):
-    return render(request, "birth/index.html")
+    return render(request, "birth/home.html")
